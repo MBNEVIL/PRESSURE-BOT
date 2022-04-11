1db266332b70fdac20244990ba3d9f0a43df068b
MBNEVIL <103430419+MBNEVIL@users.noreply.github.com>
2022-04-12 04:54:10 +0700

    Add files via upload

---
+++ pressurebot.py
@@ -0,0 +1,172 @@
+import os, discord
+import colorama
+
+from colorama.ansi import Fore
+from discord.ext import commands
+from colorama import Fore, Style
+
+token = input("Enter User Token: ")
+os.system("cls")
+os.system(f'mode 125,18')
+
+prefix = "."
+
+client = discord.Client()
+client = commands.Bot(command_prefix=prefix, self_bot=True)
+client.remove_command('help') 
+
+
+
+
+os.system("title pressure bot | dm mbn evil#9731 for help")
+
+
+print(f'''
+
+        {Fore.WHITE}{Style.BRIGHT} ▄▀▀▄▀▀▀▄  ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄  ▄▀▀▀▀▄  ▄▀▀▄ ▄▀▀▄  ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄▄▄      ▄▀▀█▄▄   ▄▀▀▀▀▄   ▄▀▀▀█▀▀▄ 
+        {Fore.WHITE}{Style.BRIGHT}█   █   █ █   █   █ ▐  ▄▀   ▐ █ █   ▐ █ █   ▐ █   █    █ █   █   █ ▐  ▄▀   ▐     ▐ ▄▀   █ █      █ █    █  ▐ 
+        {Fore.WHITE}{Style.BRIGHT}▐  █▀▀▀▀  ▐  █▀▀█▀    █▄▄▄▄▄     ▀▄      ▀▄   ▐  █    █  ▐  █▀▀█▀    █▄▄▄▄▄        █▄▄▄▀  █      █ ▐   █     
+        {Fore.WHITE}{Style.BRIGHT}   █       ▄▀    █    █    ▌  ▀▄   █  ▀▄   █    █    █    ▄▀    █    █    ▌        █   █  ▀▄    ▄▀    █      
+        {Fore.WHITE}{Style.BRIGHT} ▄▀       █     █    ▄▀▄▄▄▄    █▀▀▀    █▀▀▀      ▀▄▄▄▄▀  █     █    ▄▀▄▄▄▄        ▄▀▄▄▄▀    ▀▀▀▀    ▄▀       
+        {Fore.WHITE}{Style.BRIGHT}█         ▐     ▐    █    ▐    ▐       ▐                 ▐     ▐    █    ▐       █    ▐            █         
+        {Fore.WHITE}{Style.BRIGHT}▐                    ▐                                              ▐            ▐                 ▐         
+
+
+type "suck" in chat to pressure
+''')
+@client.event
+async def on_connect():
+    print("Logged in as: {}".format(client.user))
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.content.endswith('suck'):
+      
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('YOU')
+        await message.channel.send ('SUCK')
+        await message.channel.send ('U')
+        await message.channel.send ('CANT')
+        await message.channel.send ('LAST')
+        await message.channel.send ('WEAK ASS')
+        await message.channel.send ('BITCH')
+        await message.channel.send ('COME FOLD')
+        await message.channel.send ('U CANT PRESSURE ME')
+        await message.channel.send ('SLOW')
+        await message.channel.send ('ASS')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('LO')
+        await message.channel.send ('L')
+        await message.channel.send ('LOOLLOL')
+        await message.channel.send ('LOLL')
+        await message.channel.send ('TYPE')
+        await message.channel.send ('FASTER')
+        await message.channel.send ('WEAK')
+        await message.channel.send ('ASS')
+        await message.channel.send ('FUCK BOY')
+        await message.channel.send ('LOO WHITE BOY')
+        await message.channel.send ('IM COMIN FOR YO NECK')
+        await message.channel.send (':knife: :knife: :knife: ')
+        await message.channel.send ('COME')
+        await message.channel.send ('JVC')
+        await message.channel.send ('WEAK')
+        await message.channel.send ('RETARD')
+        await message.channel.send ('U WONT LAST')
+        await message.channel.send ('WEAK FUCK')
+        await message.channel.send ('TYPE')
+        await message.channel.send ('FASTER')
+        await message.channel.send ('DUMB')
+        await message.channel.send ('ASS')
+        await message.channel.send ('BITCH')
+        await message.channel.send ('LOLO')
+        await message.channel.send ('LOOL')
+        await message.channel.send ('OLOL')
+        await message.channel.send ('LOLO')
+        await message.channel.send ('DONT STOP')
+        await message.channel.send ('SPAMMING')
+        await message.channel.send ('WEAK ASS BITCH')
+        await message.channel.send ('LO')
+        await message.channel.send ('LOOL')
+        await message.channel.send ('LOLO')
+        await message.channel.send ('LO')
+        await message.channel.send ('TYPE')
+        await message.channel.send ('FASTER')  
+        await message.channel.send ('FATASS ')
+        await message.channel.send ('YA GRANDMA GOT A BALD FADE') 
+        await message.channel.send ('LOO')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('UR')
+        await message.channel.send ('ASS')
+        await message.channel.send ('TYPE')
+        await message.channel.send ('FASTER')
+        await message.channel.send ('YOU')
+        await message.channel.send ('FUCKING')
+        await message.channel.send ('DORITO')
+        await message.channel.send ('BAG')
+        await message.channel.send ('YO')
+        await message.channel.send ('ASS')
+        await message.channel.send ('BUILT')
+        await message.channel.send ('LIKE')
+        await message.channel.send ('BATMANS')
+        await message.channel.send ('LEFT')
+        await message.channel.send ('TESTICLE')
+        await message.channel.send ('YOU')
+        await message.channel.send ('GAY')
+        await message.channel.send ('ASS') 
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('LOOL WHY')
+        await message.channel.send ('ARE')
+        await message.channel.send ('YOU')
+        await message.channel.send ('EVEN')
+        await message.channel.send ('TRYING')
+        await message.channel.send ('WEAK')
+        await message.channel.send ('ASS')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('LOL')
+        await message.channel.send ('OL')
+        await message.channel.send ('L')
+        await message.channel.send ('YOU')
+        await message.channel.send ('CANT')
+        await message.channel.send ('PRESSURE')
+        await message.channel.send ('TYPE')
+        await message.channel.send ('FASTER')
+        await message.channel.send ('SLOW')
+        await message.channel.send ('FUCK')
+        await message.channel.send ('ROBLOX')
+        await message.channel.send ('EMO')
+        await message.channel.send ('ASS')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('YOU')
+        await message.channel.send ('PLAY')
+        await message.channel.send ('FRUIT NINJAS')
+        await message.channel.send ('WITH')
+        await message.channel.send ('YOUR')
+        await message.channel.send ('WRIST')
+        await message.channel.send ('GAY')
+        await message.channel.send ('ASS')
+        await message.channel.send ('FEMBOY')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('LOOOL')
+        await message.channel.send ('LOLOLOLO')
+        await message.channel.send ('IM')
+        await message.channel.send ('YOUR')
+        await message.channel.send ('GOD')
+        await message.channel.send ('LIL')
+        await message.channel.send ('MIDGET')
+        await message.channel.send ('BITCH')
+        await message.channel.send ('BUILT')
+        await message.channel.send ('LIKE')
+        await message.channel.send ('THE')
+        await message.channel.send ('DISCORD')
+        await message.channel.send ('LOGO')
+        await message.channel.send ('YOU')
+        await message.channel.send ('FAT')
+        await message.channel.send ('ASS')
+        await message.channel.send ('NIGGA')
+        await message.channel.send ('LZZZ LOLOL')
+
+
+
+
+ 
+client.run(token, bot=False)
